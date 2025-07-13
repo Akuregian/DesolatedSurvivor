426cb92dc18473ab6feff437b17efeeb5deda064
Anthony Kuregian <AnthonyKuregian@yahoo.com>
2025-07-13 12:55:23 -0700

    Enemies now attach the player

--- Source/DesolatedSurvivor/Characters/DesolatedSurvivorPlayerCharacter.cpp
+++ Source/DesolatedSurvivor/Characters/DesolatedSurvivorPlayerCharacter.cpp
@@ -5,6 +5,8 @@
 
 ADesolatedSurvivorPlayerCharacter::ADesolatedSurvivorPlayerCharacter()
 {
+	/* 'Player' Tag is used for Enemy AI, without it the enemy won't look for you */
+	Tags.Add(FName("Player"));
 }
 
 void ADesolatedSurvivorPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
